c674e418164e4b2398857eca0f8d89328f097000
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-06-24 10:01:12 +0100

    Update lumen images
    - Download+Install latest CMake on all images
    - Upgrade to latest Arch base image
    - Corrected Debian 11.11 base image
    - Corrected Ubuntu 22.04 base image

---
+++ scripts/install_cmake.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Download and install latest CMake 
+CMAKE_VERSION=4.0.3
+CMAKE_ARCH=$(uname -m)
+CMAKE_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+CMAKE_TARBALL=cmake-${CMAKE_VERSION}-${CMAKE_OS}-${CMAKE_ARCH}.tar.gz
+CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_TARBALL}
+
+# Download CMake
+curl -SL ${CMAKE_URL} -o /tmp/${CMAKE_TARBALL}
+# Extract CMake
+mkdir -p /opt/cmake
+tar -xzf /tmp/${CMAKE_TARBALL} -C /opt/cmake --strip-components=1
+/opt/cmake/bin/cmake --version
+# Clean up
+rm /tmp/${CMAKE_TARBALL}

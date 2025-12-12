ac9281be792fa3444a04cfc3557afe008fa789ca
Marcos Bento <Marcos.Bento@ecmwf.int>
2025-12-12 09:59:10 +0000

    Upgrade CMake to 4.2.1

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Download and install latest CMake 
-CMAKE_VERSION=4.0.3
+CMAKE_VERSION=4.2.1
 CMAKE_ARCH=$(uname -m)
 CMAKE_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 CMAKE_TARBALL=cmake-${CMAKE_VERSION}-${CMAKE_OS}-${CMAKE_ARCH}.tar.gz

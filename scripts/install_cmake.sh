6fe4a5faf92a3134bf23038664d84544c87b64be
Marcos Bento <Marcos.Bento@ecmwf.int>
2026-01-12 10:54:50 +0000

    Check CMake download with sha256sum

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -7,14 +7,20 @@ set -x
 CMAKE_VERSION=4.2.1
 CMAKE_ARCH=$(uname -m)
 CMAKE_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-CMAKE_TARBALL=cmake-${CMAKE_VERSION}-${CMAKE_OS}-${CMAKE_ARCH}.tar.gz
-CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_TARBALL}
+CMAKE_PACKAGE=cmake-${CMAKE_VERSION}-${CMAKE_OS}-${CMAKE_ARCH}.tar.gz
+CMAKE_SHA=cmake-${CMAKE_VERSION}-SHA-256.txt
+CMAKE_PACKAGE_URL=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_PACKAGE}
+CMAKE_SHA_URL=https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_SHA}
 
 # Download CMake
-curl -SL ${CMAKE_URL} -o /tmp/${CMAKE_TARBALL}
+wget ${CMAKE_PACKAGE_URL} -O /tmp/${CMAKE_PACKAGE}
+# Download CMake SHA256 checksum
+wget ${CMAKE_SHA_URL} -O /tmp/${CMAKE_SHA}
+
 # Extract CMake
 mkdir -p /opt/cmake
-tar -xzf /tmp/${CMAKE_TARBALL} -C /opt/cmake --strip-components=1
+tar -xzf /tmp/${CMAKE_PACKAGE} -C /opt/cmake --strip-components=1
 /opt/cmake/bin/cmake --version
 # Clean up
-rm /tmp/${CMAKE_TARBALL}
+rm /tmp/${CMAKE_PACKAGE}
+rm /tmp/${CMAKE_SHA}
